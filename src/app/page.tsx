c5a6c5216a343e7e5f513475fe720afb51553f0c
Yudhishthirr <officialyudhishthir@gmail.com>
2026-02-01 09:25:55 +0530

    setup background jobs with inggest

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <h1>Welcome to My Next.js App</h1>
+    <UserButton/>
+  </div>
+  );
+}
